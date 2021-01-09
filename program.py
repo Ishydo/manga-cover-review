73f466db767dcf6c47432bea1596578a9e4f7e0e
Dominique Ducommun <dducommun@infinit8.io>
2021-01-09 23:44:20 +0100

    Add animation and pertinent top 3 podium items and links

--- program.py
+++ program.py
@@ -1,7 +1,7 @@
 import requests
 import sqlite3
 
-from flask import Flask,render_template, send_from_directory, request, g
+from flask import Flask,render_template, send_from_directory, request, g, redirect,url_for
 from datetime import datetime
 from bs4 import BeautifulSoup
 
@@ -23,13 +23,14 @@ def send_js(path):
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/<volume>', methods=['GET', 'POST'])
-def hello_world(volume=1):
+def manga_cover_review(volume=1):
 
     manga = MANGA_NAME
     volume_number = volume
     next_volume_number = "{0}".format(int(volume) + 1)
     previous_volume_number = "{0}".format(int(volume) - 1)
     cover = get_single_cover(COVERS_URL, volume_number)
+    top_3 = get_top()
 
     if request.method == 'GET':
         t = (MANGA_NAME, volume_number)
@@ -49,17 +50,19 @@ def hello_world(volume=1):
 
         db = get_db()
         c = db.cursor()
-        c.execute("INSERT INTO reviews VALUES ('{}','{}',{},{},{},{})".format(
+        c.execute("INSERT INTO reviews VALUES ('{}','{}',{},'{}',{},{},{})".format(
            datetime.now(),
            MANGA_NAME,
            volume_number,
+           cover,
            note_1,
            note_2,
            note_3 
         ))
         db.commit()
 
-        return render_template('index.html', **locals())
+        next_volume_number = "{0}".format(int(volume_number) + 1)
+        return redirect(url_for('manga_cover_review', volume=next_volume_number))
 
 
 
@@ -108,12 +111,24 @@ def init_db():
              (date datetime, 
              manga varchar(255), 
              volume int,
+             cover_url text,
              note_1 float, 
              note_2 float, 
              note_3 float)''')
         db.commit()
 
 
+def get_top(podium=3):
+    t = (MANGA_NAME,)
+    db = get_db()
+    db.row_factory = sqlite3.Row
+    c= db.cursor()
+    c.execute('SELECT *, ((note_1 + note_2 + note_3) / 3.0) as avg FROM reviews WHERE manga=? ORDER BY avg DESC LIMIT 3', t)
+    entries = c.fetchall()
+    return entries
+
+
 if __name__ == "__main__":
     
-    init_db()
+    #init_db()
+    get_top()
